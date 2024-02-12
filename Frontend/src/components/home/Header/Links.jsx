0552656e3110b6ae7878e467087b79c29003d030
ChathuraSudaraka <Chathurabro68@gmail.com>
2024-02-12 15:30:39 +0530

    sum bug fix

--- Frontend/src/components/home/Header/Links.jsx
+++ Frontend/src/components/home/Header/Links.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import useApiFetch from "../../../hooks/useApiFetch";
 import Cookies from "js-cookie";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const NavLink = ({ to, text }) => (
   <Link to={to}>
@@ -13,8 +14,10 @@ const NavLink = ({ to, text }) => (
 
 const Links = () => {
   const [loading, setLoading] = useState(true);
+  const [logoutLoading, setLogoutLoading] = useState(false);
   const [loggedIn, setLoggedIn] = useState(false);
   const [user, setUser] = useState({});
+  const navigateTo = useNavigate();
 
   useEffect(() => {
     const storedUser = JSON.parse(localStorage.getItem("user"));
@@ -29,6 +32,7 @@ const Links = () => {
   }, []);
 
   function getUser() {
+    setLoading(true);
     useApiFetch({
       method: "GET",
       url: "/user",
@@ -38,58 +42,65 @@ const Links = () => {
 
         setLoggedIn(true);
         setUser(res?.user);
-        setLoading(false);
 
-        // Store user data in local storage
         localStorage.setItem("user", JSON.stringify(res?.user));
+
+        setLoading(false);
+      },
+      error: () => {
+        setLoading(false);
       },
     });
   }
 
   function logout() {
+    setLogoutLoading(true);
+
     useApiFetch({
       method: "POST",
       url: "/logout",
       body: {},
       success: () => {
-        // Remove user data from local storage
         localStorage.removeItem("user");
         Cookies.remove("token");
         setLoggedIn(false);
-        setLoading(false);
+        setLogoutLoading(false);
+        navigateTo("/signin");
+      },
+      error: () => {
+        setLogoutLoading(false);
       },
     });
   }
 
   return (
     <>
-      {loading ? (
-        <div>Loading...</div>
-      ) : (
+      {loggedIn ? (
         <>
-          {!loggedIn ? (
+          {loading && (
+            <div className="loading-indicator">
+              <CircularProgress size="24px" color="primary" />
+            </div>
+          )}
+          {user.role_id === 1 && (
             <>
-              <NavLink to="/signin" text="Login" />
-              <NavLink to="/signup" text="Sign Up" />
-            </>
-          ) : (
-            <>
-              {user.role_id === 1 && (
-                <>
-                  <NavLink to="/profile" text="Admin Profile" />
-                  <NavLink to="/admin" text="Admin Panel" />
-                  <NavLink to="/payment" text="Admin Other" />
-                </>
-              )}
-              {user.role_id === 2 && <NavLink to="/profile" text="Profile" />}
-              <li
-                className="text-gray-400 px-4 py-1 border-b-[1px] border-b-gray-400 hover:border-b-white hover:text-white duration-300 cursor-pointer"
-                onClick={logout}
-              >
-                Log Out
-              </li>
+              <NavLink to="/profile" text="Admin Profile" />
+              <NavLink to="/admin" text="Admin Panel" />
+              <NavLink to="/payment" text="Admin Other" />
             </>
           )}
+          {user.role_id === 2 && <NavLink to="/profile" text="Profile" />}
+          <li
+            className="text-gray-400 px-4 py-1 border-b-[1px] border-b-gray-400 hover:border-b-white hover:text-white duration-300 cursor-pointer"
+            onClick={logout}
+          >
+            {logoutLoading ? "Logging Out..." : "Log Out"}
+          </li>
+        </>
+      ) : (
+        <>
+          <NavLink to="/signin" text="Login" />
+          <NavLink to="/signup" text="Sign Up" />
         </>
       )}
     </>
